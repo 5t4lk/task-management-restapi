a5edd9e9c4f50d43b1c2002a371ff7d6e2c6b65d
Vladimir Shevchuk <5t4lk.dev@gmail.com>
2023-06-02 22:16:13 +0100

    initializated service for user signing up

--- internal/service/service.go
+++ internal/service/service.go
@@ -1,10 +1,20 @@
 package service
 
-import "task-management/internal/repository"
+import (
+	"task-management/internal/repository"
+	"task-management/internal/types"
+)
+
+type Authorization interface {
+	CreateUser(user types.User) (int, error)
+}
 
 type Service struct {
+	Authorization
 }
 
 func NewService(repos *repository.Repository) *Service {
-	return &Service{}
+	return &Service{
+		Authorization: NewAuthService(repos.Authorization),
+	}
 }
