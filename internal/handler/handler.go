bb7b2096cdbbfbfe6123377636ad7422bc40ea5e
Vladimir Shevchuk <5t4lk.dev@gmail.com>
2023-06-02 20:15:43 +0100

    created a base for handler

--- internal/handler/handler.go
+++ internal/handler/handler.go
@@ -1,1 +1,18 @@
 package handler
+
+import (
+	"github.com/gin-gonic/gin"
+	"task-management/internal/service"
+)
+
+type Handler struct {
+	services *service.Service
+}
+
+func NewHandler(services *service.Service) *Handler {
+	return &Handler{services: services}
+}
+
+func (h *Handler) InitRoutes() *gin.Engine {
+
+}
