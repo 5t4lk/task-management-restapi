89a900843a04bdfd92b0826cdb064947246d3642
Vladimir Shevchuk <5t4lk.dev@gmail.com>
2023-06-02 21:47:32 +0100

    wrote mysql base in file mysql.go

---
+++ internal/repository/database/mysql.go
@@ -0,0 +1,29 @@
+package database
+
+import (
+	"fmt"
+	"github.com/jmoiron/sqlx"
+)
+
+type Config struct {
+	Host     string
+	Port     string
+	Username string
+	Password string
+	DBName   string
+	SSLMode  string
+}
+
+func NewMySQLDb(cfg Config) (*sqlx.DB, error) {
+	db, err := sqlx.Open("mysql", fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=%s",
+		cfg.Host, cfg.Port, cfg.Username, cfg.Password, cfg.DBName, cfg.SSLMode))
+	if err != nil {
+		return nil, err
+	}
+
+	if err = db.Ping(); err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
