9d1e7794ef89631e4bee8f09dbb665f2fccd6aaf
Vladimir Shevchuk <5t4lk.dev@gmail.com>
2023-06-02 22:16:56 +0100

    wrote auth.go to service for user signing-up

--- internal/service/auth.go
+++ internal/service/auth.go
@@ -1,1 +1,18 @@
 package service
+
+import (
+	"task-management/internal/repository"
+	"task-management/internal/types"
+)
+
+type AuthService struct {
+	repo repository.Authorization
+}
+
+func NewAuthService(repo repository.Authorization) *AuthService {
+	return &AuthService{repo: repo}
+}
+
+func (a *AuthService) CreateUser(user types.User) (int, error) {
+	return a.repo.CreateUser(user)
+}
