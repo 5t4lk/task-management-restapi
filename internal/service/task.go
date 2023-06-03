234d99bddc8e3eab9f0522a0b76aadef5567e7d9
Vladimir Shevchuk <5t4lk.dev@gmail.com>
2023-06-03 18:31:10 +0100

    realized interface via adding method GetAll in service

--- internal/service/task.go
+++ internal/service/task.go
@@ -16,3 +16,7 @@ func NewTaskService(repo repository.Task) *TaskService {
 func (t *TaskService) Create(userId int, task types.Task) (int, error) {
 	return t.repo.Create(userId, task)
 }
+
+func (t *TaskService) GetAll(userId int) ([]types.Task, error) {
+	return t.repo.GetAll(userId)
+}
