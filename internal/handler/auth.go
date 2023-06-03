c9004b18d340088a4e0b1047dcb3f791fb5d5cd7
Vladimir Shevchuk <5t4lk.dev@gmail.com>
2023-06-03 16:50:05 +0100

    created handler for user signing in

--- internal/handler/auth.go
+++ internal/handler/auth.go
@@ -26,5 +26,20 @@ func (h *Handler) signUp(c *gin.Context) {
 }
 
 func (h *Handler) signIn(c *gin.Context) {
+	var input types.UserSignIn
 
+	if err := c.BindJSON(&input); err != nil {
+		newErrorResponse(c, http.StatusBadRequest, err.Error())
+		return
+	}
+
+	token, err := h.services.Authorization.GenerateToken(input.Username, input.Password)
+	if err != nil {
+		newErrorResponse(c, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	c.JSON(http.StatusOK, map[string]interface{}{
+		"token": token,
+	})
 }
