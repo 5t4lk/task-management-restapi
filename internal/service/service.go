988187c5884c7e40f3bf0a06fdf8f6f9a0d7cd47
Vladimir Shevchuk <5t4lk.dev@gmail.com>
2023-06-03 17:01:05 +0100

    added method ParseToken to Authorization interface

--- internal/service/service.go
+++ internal/service/service.go
@@ -8,6 +8,7 @@ import (
 type Authorization interface {
 	CreateUser(user types.User) (int, error)
 	GenerateToken(username, password string) (string, error)
+	ParseToken(token string) (string, error)
 }
 
 type Service struct {
