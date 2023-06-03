68cf5b52d914ba747d6c65420b822a814c3220ca
Vladimir Shevchuk <5t4lk.dev@gmail.com>
2023-06-03 12:16:07 +0100

    deleted constructor for server structure and moved config to func Run()

--- internal/server/server.go
+++ internal/server/server.go
@@ -10,19 +10,14 @@ type Server struct {
 	httpServer *http.Server
 }
 
-func NewServer(port string, handler http.Handler) *Server {
-	return &Server{
-		httpServer: &http.Server{
-			Addr:           ":" + port,
-			Handler:        handler,
-			ReadTimeout:    10 * time.Second,
-			WriteTimeout:   10 * time.Second,
-			MaxHeaderBytes: 1 << 20,
-		},
+func (s *Server) Run(port string, handler http.Handler) error {
+	s.httpServer = &http.Server{
+		Addr:           ":" + port,
+		Handler:        handler,
+		ReadTimeout:    10 * time.Second,
+		WriteTimeout:   10 * time.Second,
+		MaxHeaderBytes: 1 << 20,
 	}
-}
-
-func (s *Server) Run() error {
 	return s.httpServer.ListenAndServe()
 }
 
