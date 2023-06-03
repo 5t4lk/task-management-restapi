816074fccedc11849ba7e54be715f026c1983e6d
Vladimir Shevchuk <5t4lk.dev@gmail.com>
2023-06-03 17:48:23 +0100

    created handler for creating tasks

--- internal/handler/task.go
+++ internal/handler/task.go
@@ -1,9 +1,33 @@
 package handler
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"task-management/internal/types"
+)
 
 func (h *Handler) createTask(c *gin.Context) {
+	userId, err := getUserId(c)
+	if err != nil {
+		newErrorResponse(c, http.StatusInternalServerError, err.Error())
+		return
+	}
 
+	var input types.Task
+	if err := c.BindJSON(&input); err != nil {
+		newErrorResponse(c, http.StatusBadRequest, err.Error())
+		return
+	}
+
+	id, err := h.services.Task.Create(userId, input)
+	if err != nil {
+		newErrorResponse(c, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	c.JSON(http.StatusCreated, map[string]interface{}{
+		"id": id,
+	})
 }
 
 func (h *Handler) getAllTasks(c *gin.Context) {
