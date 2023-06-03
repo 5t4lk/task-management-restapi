5ee1445edf1e7f1221f96efbb1bc45d30afd60f5
Vladimir Shevchuk <5t4lk.dev@gmail.com>
2023-06-03 17:51:15 +0100

    realized Task interface with method Create

--- internal/service/task.go
+++ internal/service/task.go
@@ -1,1 +1,18 @@
 package service
+
+import (
+	"task-management/internal/repository"
+	"task-management/internal/types"
+)
+
+type TaskService struct {
+	repo repository.Task
+}
+
+func NewTaskService(repo repository.Task) *TaskService {
+	return &TaskService{repo: repo}
+}
+
+func (t *TaskService) Create(userId int, task types.Task) (int, error) {
+	return t.repo.Create(userId, task)
+}
