35463d724d5d971a836971412de5436daf0d3b05
Vladimir Shevchuk <5t4lk.dev@gmail.com>
2023-06-03 16:10:38 +0100

    created structures for tasking

--- internal/types/task.go
+++ internal/types/task.go
@@ -1,1 +1,30 @@
 package types
+
+type Task struct {
+	Id          int    `json:"id" db:"id"`
+	Title       string `json:"title" db:"title" binding:"required"`
+	Description string `json:"description" db:"description" binding:"required"`
+	Status      string `json:"status" db:"status"`
+	EndDate     string `json:"endDate" db:"end_date"`
+}
+
+type UsersTask struct {
+	Id     int
+	UserId int
+	TaskId int
+}
+
+type TaskItem struct {
+	Id          int    `json:"id" db:"id"`
+	Title       string `json:"title" db:"title" binding:"required"`
+	Description string `json:"description" db:"description"`
+	Status      string `json:"status" db:"status"`
+	EndDate     string `json:"endDate" db:"end_date"`
+	Done        bool   `json:"done" db:"done"`
+}
+
+type TasksItem struct {
+	Id     int
+	TaskId int
+	ItemId int
+}
