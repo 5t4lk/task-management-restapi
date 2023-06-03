2e9da53c046b74245d1fcd2e1a95f5fd698e8307
Vladimir Shevchuk <5t4lk.dev@gmail.com>
2023-06-03 12:18:53 +0100

    set logrus formatter to format JSON for comfortable handling errors

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,54 @@
 package main
 
-import "fmt"
+import (
+	"github.com/joho/godotenv"
+	"github.com/sirupsen/logrus"
+	"github.com/spf13/viper"
+	"os"
+	"task-management/internal/handler"
+	"task-management/internal/repository"
+	"task-management/internal/repository/database/mysql"
+	"task-management/internal/server"
+	"task-management/internal/service"
+)
 
 func main() {
-	fmt.Println("hello world")
+	logrus.SetFormatter(new(logrus.JSONFormatter))
+
+	if err := initConfig(); err != nil {
+		logrus.Fatalf("error occurred while initializating config: %s", err.Error())
+	}
+
+	if err := godotenv.Load(); err != nil {
+		logrus.Fatalf("error occurred while loading env variables: %s", err.Error())
+	}
+
+	db, err := mysql.NewMySQLDb(mysql.Config{
+		Host:     viper.GetString("db.host"),
+		Port:     viper.GetString("db.port"),
+		Username: viper.GetString("db.username"),
+		Password: os.Getenv("DB_PASSWORD"),
+		DBName:   viper.GetString("db.dbname"),
+		SSLMode:  viper.GetString("db.sslmode"),
+	})
+	if err != nil {
+		logrus.Fatalf("error occured while initializating database: %s", err.Error())
+	}
+
+	repos := repository.NewRepository(db)
+	services := service.NewService(repos)
+	handlers := handler.NewHandler(services)
+
+	srv := new(server.Server)
+	go func() {
+		if err := srv.Run(viper.GetString("port"), handlers.InitRoutes()); err != nil {
+			logrus.Fatalf("error occurred while running http server: %s", err.Error())
+		}
+	}()
+}
+
+func initConfig() error {
+	viper.AddConfigPath("configs")
+	viper.SetConfigFile("config")
+	return viper.ReadInConfig()
 }
