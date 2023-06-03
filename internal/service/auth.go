63b8bccbd0d0a447edac262784d09696916d4522
Vladimir Shevchuk <5t4lk.dev@gmail.com>
2023-06-03 16:52:29 +0100

    realized generation of token for user session; also generated hashing of user's password

--- internal/service/auth.go
+++ internal/service/auth.go
@@ -1,8 +1,19 @@
 package service
 
 import (
+	"crypto/sha1"
+	"fmt"
+	"github.com/dgrijalva/jwt-go"
+	"strconv"
 	"task-management/internal/repository"
 	"task-management/internal/types"
+	"time"
+)
+
+const (
+	salt       = "hjqrhjqw124617ajfhajs"
+	signingKey = "qrkjk#4#%35FSFJlja#4353KSFjH"
+	tokenTTL   = 12 * time.Hour
 )
 
 type AuthService struct {
@@ -14,5 +25,28 @@ func NewAuthService(repo repository.Authorization) *AuthService {
 }
 
 func (a *AuthService) CreateUser(user types.User) (int, error) {
+	user.Password = generatePasswordHash(user.Password)
 	return a.repo.CreateUser(user)
+}
+
+func (a *AuthService) GenerateToken(username, password string) (string, error) {
+	user, err := a.repo.GetUser(username, generatePasswordHash(password))
+	if err != nil {
+		return "", err
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.StandardClaims{
+		Subject:   strconv.Itoa(user.Id),
+		IssuedAt:  time.Now().Unix(),
+		ExpiresAt: time.Now().Add(tokenTTL).Unix(),
+	})
+
+	return token.SignedString([]byte(signingKey))
+}
+
+func generatePasswordHash(password string) string {
+	hash := sha1.New()
+	hash.Write([]byte(password))
+
+	return fmt.Sprintf("%x", hash.Sum([]byte(salt)))
 }
