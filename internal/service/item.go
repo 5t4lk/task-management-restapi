97f3b0e0d97dca28ca177cc5e2d238371604108c
Vladimir Shevchuk <5t4lk.dev@gmail.com>
2023-06-04 22:20:47 +0100

    created ItemService for items

--- internal/service/item.go
+++ internal/service/item.go
@@ -1,1 +1,18 @@
 package service
+
+import (
+	"task-management/internal/repository"
+	"task-management/internal/types"
+)
+
+type ItemService struct {
+	repo repository.Item
+}
+
+func NewItemService(repo repository.Item) *ItemService {
+	return &ItemService{repo: repo}
+}
+
+func (i *ItemService) Create(userId, taskId int, item types.TaskItem) (int, error) {
+	return i.repo.Create(taskId, item)
+}
