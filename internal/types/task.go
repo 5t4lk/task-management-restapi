81c1ec866f10ed8a5b4204ba8029d74c5ce1fb8c
Vladimir Shevchuk <5t4lk.dev@gmail.com>
2023-06-04 22:21:48 +0100

    fixed some bugs with item structure

--- internal/types/task.go
+++ internal/types/task.go
@@ -18,9 +18,6 @@ type TaskItem struct {
 	Id          int    `json:"id" db:"id"`
 	Title       string `json:"title" db:"title" binding:"required"`
 	Description string `json:"description" db:"description"`
-	Status      string `json:"status" db:"status"`
-	EndDate     string `json:"endDate" db:"end_date"`
-	Done        bool   `json:"done" db:"done"`
 }
 
 type TasksItem struct {
