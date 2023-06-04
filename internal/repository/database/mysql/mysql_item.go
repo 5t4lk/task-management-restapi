37644b33304945a32b0e720ba8c2f83df3df522d
Vladimir Shevchuk <5t4lk.dev@gmail.com>
2023-06-04 22:19:56 +0100

    added a method for creating item for mysql

--- internal/repository/database/mysql/mysql_item.go
+++ internal/repository/database/mysql/mysql_item.go
@@ -1,1 +1,44 @@
 package mysql
+
+import (
+	"fmt"
+	"github.com/jmoiron/sqlx"
+	"task-management/internal/types"
+)
+
+type itemMySQL struct {
+	db *sqlx.DB
+}
+
+func NewItemMySQL(db *sqlx.DB) *itemMySQL {
+	return &itemMySQL{db: db}
+}
+
+func (i *itemMySQL) Create(taskId int, item types.TaskItem) (int, error) {
+	tx, err := i.db.Begin()
+	if err != nil {
+		return 0, err
+	}
+
+	createItemQuery := fmt.Sprintf("INSERT INTO %s (title, description) VALUES (?, ?)", itemsTable)
+	result, err := tx.Exec(createItemQuery, item.Title, item.Description)
+	if err != nil {
+		tx.Rollback()
+		return 0, err
+	}
+
+	itemId, err := result.LastInsertId()
+	if err != nil {
+		tx.Rollback()
+		return 0, err
+	}
+
+	createTaskItemsQuery := fmt.Sprintf("INSERT INTO %s (task_id, item_id) VALUES (?, ?)", tasksItemsTable)
+	_, err = tx.Exec(createTaskItemsQuery, taskId, itemId)
+	if err != nil {
+		tx.Rollback()
+		return 0, err
+	}
+
+	return int(itemId), nil
+}
