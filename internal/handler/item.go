b754d1efd69f2ad6f0fca3b97c880c319849d5d5
Vladimir Shevchuk <5t4lk.dev@gmail.com>
2023-06-04 22:19:05 +0100

    created handler for creating item

--- internal/handler/item.go
+++ internal/handler/item.go
@@ -1,1 +1,38 @@
 package handler
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"strconv"
+	"task-management/internal/types"
+)
+
+func (h *Handler) createItem(c *gin.Context) {
+	userId, err := getUserId(c)
+	if err != nil {
+		newErrorResponse(c, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	taskId, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		newErrorResponse(c, http.StatusBadRequest, "invalid task id param")
+		return
+	}
+
+	var input types.TaskItem
+	if err := c.BindJSON(&input); err != nil {
+		newErrorResponse(c, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	id, err := h.services.Item.Create(userId, taskId, input)
+	if err != nil {
+		newErrorResponse(c, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	c.JSON(http.StatusOK, map[string]interface{}{
+		"id": id,
+	})
+}
